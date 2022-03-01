03419f41e0e1b049863c6fb9d8f1d0929efe1eeb
maiaatala <ana.atala@unemat.br>
2022-02-28 20:05:02 -0400

    cont

--- tutorial-app/src/06miniBookList.js
+++ tutorial-app/src/06miniBookList.js
@@ -1,43 +1,67 @@
 // CSS out of .js
-import "./03miniBook.css";
-
+import "./06miniBookList.css";
+// lists in react need a key item field
 const books = [
     {
+        id: 1,
         img: "https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L.jpg",
+        alt: "I Love You to the Moon and Back cover image",
         title: "I Love You to the Moon and Back",
         author: "Amelia Hepworth",
+        desc: "something to add",
     },
     {
+        id: 2,
         img: "https://images-na.ssl-images-amazon.com/images/I/81-V7skoi6L.jpg",
+        alt: "Food IQ Cover Image",
         title: "Food IQ: 100 Questions, Answers, and Recipes to Raise Your Cooking Smarts",
         author: "Daniel Holzman",
     },
+    {
+        id: 3,
+        img: "https://images-na.ssl-images-amazon.com/images/I/61NdJMwAThS.jpg",
+        title: "The Body Keeps the Score",
+        author: "Bessel van der Kolk M.D.",
+    },
 ];
 // you can simply iterrate string arrays, but iterate objects are not as simple
-const names = ["john", "peter", "susan"]; //arays are const?
-const newNames = names.map((name) => {
-    return <h1>{name}</h1>;
-});
+// const names = ["john", "peter", "susan"]; //arays are const?
+/* .map() accesss each element.
+elementName =>{return} */
+// const newNames = names.map((name) => {
+//     return <h1>{name}</h1>;
+// });
 
 export default function BookList() {
+    /* first {} to acess javascript inside html
+    second {} to write css using javascript
+    you change - to camelCaseWords
+    all values must be in ""
+    inLineCSS > .css file when rendering */
     return (
         <section className="booklist" style={{ alignItems: "flex-start" }}>
             {/* makes the the bottom of the grid self aling */}
-            {newNames}
+            {/* {newNames} */}
+            {books.map((book) => {
+                // const { img, alt, title, author, children } = book;
+                return <Book key={book.id} {...book} />;
+                // this is how react knows which is the key
+                // ... is a SPREAD OPERATOR
+            })}
         </section>
     );
 }
 
-const Book = ({ img, title, author, children }) => {
-    // CHILDREN NAME MATTERS
+const Book = (props) => {
+    const { img, alt, title, author } = props; // names must match with the object
+    // console.log(img);
     // const { img, title, author } = props; //descontructing
+
     return (
         <article className="book">
-            <Image image={img}></Image>
+            <Image image={img} alt={alt}></Image>
             <Title title={title} />
             <Author name={author} />
-            {children}
-            {/* {props.children} */}
         </article>
     );
 };
@@ -63,17 +87,7 @@ const Author = (props) => {
     );
 };
 
-/* 
-first {} to acess javascript inside html
-second {} to write css using javascript
-you change - to camelCaseWords
-all values must be in ""
-inLineCSS > .css file when rendering
-*/
-
 const Image = (props) => {
-    return (
-        <img src={props.image} alt="moon and back cover" className="image" />
-    );
+    return <img src={props.image} alt={props.alt} className="image" />;
     // even the img tag needs to be closed off
 };

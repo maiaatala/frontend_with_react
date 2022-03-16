9ef1df724c9c28c50e0902a95d3c6a35144ae7cf
maiaatala <ana.atala@unemat.br>
2022-03-16 16:43:19 -0400

    input form

--- 02-react-hooks/src/tutorial/4-conditionalRendering/2-shorCircuit.js
+++ 02-react-hooks/src/tutorial/4-conditionalRendering/2-shorCircuit.js
@@ -2,10 +2,10 @@ import React, { useState, useEffect } from "react";
 
 const ShortCircuit = () => {
   const [text, setText] = useState("");
+  const [inpText, setInpText] = useState("");
 
-  const firstValue = text || "hello world";
+  /*   const firstValue = text || "hello world";
   const secondValue = text && "hello world2";
-
   /* if text = false 
     firstValue = "Hello World"
     secondValue = ""
@@ -14,26 +14,33 @@ const ShortCircuit = () => {
     secondValue = "Hello World"
   */
 
+  const handleSubmit = (e) => {
+    e.preventDefault(); // ! PRECISA DISSO SEMPRE
+    setText(inpText);
+  };
+
+  const handleChange = (e) => {
+    setInpText(e.target.value);
+  };
+
   return (
     <>
       {/* <h1>v1: {firstValue}</h1>
       <h1 style={{ margin: "0" }}>v2: {secondValue}</h1> */}
-      <h1>{text || "Under Construction"}</h1>
-      <h2>{!text && "try again later..."}</h2>
-      <h2>{text && "My Site"}</h2>
-      <button
-        className="btn"
-        onClick={() => {
-          setText((prev) => {
-            if (!prev) {
-              return "John Doe";
-            }
-            return "";
-          });
-        }}
-      >
-        change txt
-      </button>
+      <div style={{ minHeight: "100px" }}>
+        <h1>{text || "Under Construction"}</h1>
+        <h2>{!text && "Tell me your name..."}</h2>
+        <h2>{text && "My Site"}</h2>
+      </div>
+      <div className="Forms">
+        <form onSubmit={handleSubmit}>
+          {/* <label>Name: </label> */}
+          <input type="text" value={inpText} onChange={handleChange} />
+          <button className="btn" onClick={handleSubmit}>
+            apply
+          </button>
+        </form>
+      </div>
     </>
   );
 };
